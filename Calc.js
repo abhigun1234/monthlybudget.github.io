33c84baaedc27db43b7041f5e51d18b77e0102e0
abhigun1234 <37846168+abhigun1234@users.noreply.github.com>
2023-02-18 20:34:27 +0530

    first commit

---
+++ Calc.js
@@ -0,0 +1,19 @@
+    /**
+     * 
+     * 
+     * let  start with building a emi calculator
+     * 
+     * 
+     */
+
+    var homeLoan=Number(prompt("enter your home loan emi"))
+    var carLoan=Number(prompt("enter your car loan emi"))
+    var bikeLoan=Number(prompt("enter your car loan emi"))
+    salary=50000;
+
+    // add all expenses 
+
+    totalEmi=homeLoan+carLoan+bikeLoan;
+
+    console.log("totalEmi",totalEmi)
+
