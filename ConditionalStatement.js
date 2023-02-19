2de944bf408bf7b707ded2495638e6a078ccca62
abhigun1234 <37846168+abhigun1234@users.noreply.github.com>
2023-02-19 11:21:19 +0530

    add

---
+++ ConditionalStatement.js
@@ -0,0 +1,16 @@
+// == >= <= === != 
+var a=10;
+var b='10';
+if(a===b){
+    console.log("a and  b are equal")
+}
+else{
+    console.log("they are not equal")
+}
+// a b c check which one is greter
+// login page 
+// if email id = tourindia@gmail.com
+// passwors=india123456
+
+// land into home page
+// show login failure
